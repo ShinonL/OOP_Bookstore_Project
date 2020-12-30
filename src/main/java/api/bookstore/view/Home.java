ff82f704fc735f6101c20d5434780773ec4356a1
Matei Octavian <mateitavis@gmail.com>
2020-12-30 15:33:11 +0200

    NavBar + Home(started)

--- src/main/java/api/bookstore/view/Home.java
+++ src/main/java/api/bookstore/view/Home.java
@@ -4,23 +4,69 @@ import javax.swing.*;
 
 import org.springframework.stereotype.Component;
 
+import java.awt.*;
+
 @Component
 public class Home extends AppFrame{
 
     @Override
     public void initialize() {
-        this.setTitle("Agenda");
-        this.setSize(400, 400);
+        this.setResizable(false);
+        this.setTitle("Bookstore");
+        this.setSize(1600, 1200);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(null);
 
         JPanel panel = new JPanel();
         panel.setLayout(null);
 
-        panel = NavigationBar.navBar(panel, this);
+        NavigationBar.navBar(panel, this);
+        panel.setBackground(Color.CYAN);
+        JPanel panelInside = new JPanel();
+        panelInside.setLayout(null);
+        panelInside.setBounds(this.getSize().width/8,2*this.getSize().height/20,
+                           3*this.getSize().width/4,this.getSize().height-30);
+        panelInside.setBackground(new Color(225, 198, 153));
+        initializeInside(panelInside);
+
+        panel.add(panelInside);
 
         this.setContentPane(panel);
         this.setVisible(true);
+    }
+
+    private void initializeInside(JPanel panelInside)
+    {
+        JPanel bestPanel = new JPanel();
+        JPanel newPanel = new JPanel();
+        JPanel promotionsPanel = new JPanel();
+
+        bestPanel.setBackground(Color.BLACK);
+        newPanel.setBackground(Color.BLACK);
+        promotionsPanel.setBackground(Color.BLACK);
+
+        bestPanel.setBounds(panelInside.getBounds().width/20,10,panelInside.getBounds().width-2*panelInside.getBounds().width/20,panelInside.getBounds().height/3-30);
+        newPanel.setBounds(panelInside.getBounds().width/20,10+panelInside.getBounds().height/3,panelInside.getBounds().width-2*panelInside.getBounds().width/20,panelInside.getBounds().height/3-30);
+        promotionsPanel.setBounds(panelInside.getBounds().width/20,2*panelInside.getBounds().height/3-10,panelInside.getBounds().width-2*panelInside.getBounds().width/20,panelInside.getBounds().height/3-30);
+
+        initializeBest(bestPanel);
+        initializeNew(newPanel);
+        initializePromotions(promotionsPanel);
+
+        panelInside.add(bestPanel);
+        panelInside.add(newPanel);
+        panelInside.add(promotionsPanel);
+    }
+
+    private void initializeBest(JPanel bestPanel)
+    {
+
+    }
+    private void initializeNew(JPanel newPanel)
+    {
+    }
+    private void initializePromotions(JPanel promotionsPanel)
+    {
     }
 
 }
--- src/main/java/api/bookstore/view/NavigationBar.java
+++ src/main/java/api/bookstore/view/NavigationBar.java
@@ -5,55 +5,63 @@ import javax.swing.JPanel;
 
 import org.springframework.stereotype.Component;
 
+import java.awt.*;
+
 @Component
 public class NavigationBar {
 
     public static JPanel navBar(JPanel myPanel, AppFrame prevFrame) {
+        JPanel panelInside = new JPanel();
+        panelInside.setLayout(null);
+        panelInside.setBounds(prevFrame.getSize().width/8,0,
+                3*prevFrame.getSize().width/4,prevFrame.getBounds().height/20+20);
+        panelInside.setBackground(new Color(225, 198, 153));
 
         JButton homeButton = new JButton("Home");
-        homeButton.setBounds(10, 10, 120, 30);
+        homeButton.setBounds(panelInside.getBounds().width/6, 10, panelInside.getBounds().width/6-10, panelInside.getBounds().height-20);
         homeButton.addActionListener(e -> {
             Home myHome = new Home();
             myHome.initialize();
             myHome.setVisible(true);
             prevFrame.setVisible(false);
         });
-        myPanel.add(homeButton);
-
-
-        JButton aboutUsButton = new JButton("AboutUS");
-        aboutUsButton.setBounds(10, 45, 120, 30);
-        aboutUsButton.addActionListener(e -> {
-            AboutUs aboutUs = new AboutUs();
-            aboutUs.initialize();
-            aboutUs.setVisible(true);
-            prevFrame.setVisible(false);
-        });
-        myPanel.add(aboutUsButton);
-
-
-        JButton myAccountButton = new JButton("My Account");
-        myAccountButton.setBounds(10, 105, 120, 30);
-        myAccountButton.addActionListener(e -> {
-            MyAccount myAccount = new MyAccount();
-            myAccount.initialize();
-            myAccount.setVisible(true);
-            prevFrame.setVisible(false);
-        });
-        myPanel.add(myAccountButton);
-
+        panelInside.add(homeButton);
 
         JButton categoryButton = new JButton("Categories");
-        categoryButton.setBounds(10, 135, 120, 30);
+        categoryButton.setBounds(2*panelInside.getBounds().width/6, 10, panelInside.getBounds().width/6-10, panelInside.getBounds().height-20);
         categoryButton.addActionListener(e -> {
             Category category = new Category();
             category.initialize();
             category.setVisible(true);
             prevFrame.setVisible(false);
         });
-        myPanel.add(categoryButton);
+        panelInside.add(categoryButton);
+
+        JButton aboutUsButton = new JButton("AboutUS");
+        aboutUsButton.setBounds(3*panelInside.getBounds().width/6, 10, panelInside.getBounds().width/6-10, panelInside.getBounds().height-20);
+        aboutUsButton.addActionListener(e -> {
+            AboutUs aboutUs = new AboutUs();
+            aboutUs.initialize();
+            aboutUs.setVisible(true);
+            prevFrame.setVisible(false);
+        });
+        panelInside.add(aboutUsButton);
 
 
+        JButton myAccountButton = new JButton("My Account");
+        myAccountButton.setBounds(4*panelInside.getBounds().width/6, 10, panelInside.getBounds().width/6-10, panelInside.getBounds().height-20);
+        myAccountButton.addActionListener(e -> {
+            MyAccount myAccount = new MyAccount();
+            myAccount.initialize();
+            myAccount.setVisible(true);
+            prevFrame.setVisible(false);
+        });
+        panelInside.add(myAccountButton);
+
+
+
+
+        myPanel.add(panelInside);
 
         return myPanel;
     }
